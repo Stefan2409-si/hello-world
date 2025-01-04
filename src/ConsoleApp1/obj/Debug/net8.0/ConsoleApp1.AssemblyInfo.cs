45454af85a5d187d8b235ff4c16bf7c921086541
Stefan Ilic <stefanilic99@gmail.com>
2025-01-04 02:28:31 +0100

    stage"

--- src/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ src/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a22a5696cccace3c8eb4660b7b94465c31fe40b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f47c17ada94f66e4c3fb3989b221e1b36ba709d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/Zadatak1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Zadatak1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int zbir = SaberiDvaBroja(a, b);
+            Console.WriteLine("Evo ga zbir " + zbir);
+        }
+        
+        static void IspisiNasumicnoIme()
+        {
+            List<string> mogucaImena = new List<string>()
+            {
+                "Marko",
+                "Stefan",
+                "aleksa",
+                "Nikola",
+                "Lazar"
+            };
+
+            Console.WriteLine(mogucaImena[System.Random.Shared.Next(mogucaImena.Count)]);
+        }
+
+        static string GenerisiNasumicnoIme()
+        {
+            List<string> mogucaImena = new List<string>()
+            {
+                "Marko",
+                "Stefan",
+                "aleksa",
+                "Nikola",
+                "Lazar"
+            };
+
+            return mogucaImena[System.Random.Shared.Next(mogucaImena.Count)];
+        }
+
+        static int SaberiDvaBroja(int prvi, int drugi)
+        {
+            Console.WriteLine("Pripremite se!!! Sabiram dva broja!!!");
+            return prvi + drugi;
+        }
+
+        static int PodeliDvaBroja(int prvi, int drugi)
+        {
+            return prvi / drugi;
+        }
+
+
+    }
+}
