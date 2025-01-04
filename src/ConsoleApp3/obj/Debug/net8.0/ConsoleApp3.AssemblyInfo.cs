2bfabfbbac4df38d32bece38d3b6e02e3b493ccc
Stefan Ilic <stefanilic99@gmail.com>
2025-01-04 02:55:46 +0100

    Stage

--- src/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ src/ConsoleApp3/obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45454af85a5d187d8b235ff4c16bf7c921086541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19f454316f99847905e67d943efae0ef109e03d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
